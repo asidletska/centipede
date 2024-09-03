564ecbe2443b88dbfef2385a1f67976237b4a2be
WIN-FI93QU5E7B9\Asidletska <nastiasidletskaya@gmail.com>
2024-09-03 22:27:54 +0300

    Mushroom

--- Assets/Scripts/Mushroom.cs
+++ Assets/Scripts/Mushroom.cs
@@ -2,5 +2,33 @@ using UnityEngine;
 
 public class Mushroom : MonoBehaviour
 {
+    public Sprite[] states;
+    private SpriteRenderer spriteRenderer;
+    private int health;
 
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        health = states.Length;
+    }
+    private void Damage(int amount)
+    {
+        health -= amount;
+
+        if (health > 0 )
+        {
+            spriteRenderer.sprite = states[states.Length - health];
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Dart"))
+        {
+            Damage(1);
+        }
+    }
 }
