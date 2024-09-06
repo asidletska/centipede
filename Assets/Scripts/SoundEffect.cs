d300ba7816c21ea7c3e3f222a4e30d20c2357536
WIN-FI93QU5E7B9\Asidletska <nastiasidletskaya@gmail.com>
2024-09-06 13:28:49 +0300

    finish

---
+++ Assets/Scripts/SoundEffect.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SoundEffect : MonoBehaviour
+{
+    public AudioSource myFx;
+    public AudioClip clickFx;
+
+    public void ClickSound()
+    {
+        myFx.PlayOneShot(clickFx);
+    }
+}
